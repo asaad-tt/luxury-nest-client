5fc89e193f6cc27631d266e42989640f22463e3f
asaad-tt <aasadshumon86@gmail.comm>
2023-02-08 17:12:51 +0600

    add home

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -14,6 +14,9 @@ import DashboardLayout from "../Layout/DashboardLayout";
 import Welcome from "../Pages/Dashboard/Welcome";
 import MyBookings from "../Pages/Dashboard/MyBookings";
 import BecomeAHost from "../Pages/Dashboard/BecomeAHost";
+import AllUsers from "../Pages/Dashboard/AllUsers";
+import AllBookings from "../Pages/Dashboard/AllBookings";
+import AddHome from "../Pages/AddHome";
 
 const router = createBrowserRouter([
   {
@@ -80,6 +83,30 @@ const router = createBrowserRouter([
         element: (
           <PrivateRoute>
             <BecomeAHost />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "all-users",
+        element: (
+          <PrivateRoute>
+            <AllUsers />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "all-bookings",
+        element: (
+          <PrivateRoute>
+            <AllBookings />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "add-home",
+        element: (
+          <PrivateRoute>
+            <AddHome />
           </PrivateRoute>
         ),
       },
--- src/api/user.js
+++ src/api/user.js
@@ -24,3 +24,30 @@ export const getRole = async (email) => {
 
   return user?.role;
 };
+
+// Get All Users
+export const getAllUsers = async () => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/users`);
+
+  const users = await response.json();
+
+  return users;
+};
+
+// approve host
+export const makeHost = async (user) => {
+  delete user._id;
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/user/${user?.email}`,
+    {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ ...user, role: "host" }),
+    }
+  );
+  const users = await response.json();
+
+  return users;
+};
